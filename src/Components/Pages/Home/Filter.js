613c3376eda40a6a63ee40b831c2f4d07e30e94a
SundarHari003 <sundarhari014@gmail.com>
2024-05-13 18:18:02 +0530

    updated new files

--- src/Components/Pages/Home/Filter.js
+++ src/Components/Pages/Home/Filter.js
@@ -61,7 +61,7 @@ const Filter = () => {
       }, []);
     
   return (
-        <div className='scrollable-container xl:px-24 lg:px-14 px-5  py-6 w-full flex overflow-y-visible overflow-x-auto items-center gap-3 scroll-bar sticky top-[7.2rem]   md:top-[7.5rem]  lg:top-[4.5rem] bg-white z-[999] '>
+        <div className='scrollable-container xl:px-24 lg:px-14 px-5  py-6 w-full flex overflow-y-visible overflow-x-auto items-center gap-3 scroll-bar sticky top-[7.2rem]   md:top-[7.5rem]  lg:top-[4.5rem] bg-white z-[998] '>
             <div className='relative'>
                 <div className={`text-base  font-semibold flex items-center bg-gray-200 ${DeliveryFees==7?"w-[16.7rem]":"w-[15.7rem]"} px-3 py-[0.1rem] h-[1.9rem] rounded-full cursor-pointer hover:bg-gray-50 hover:text-black transition-all duration-200 `}>
                     <div className=' flex text-base items-center' >Delivery Fees: Under<TbCurrencyDollar size={17}/><span className='pt-[0.2rem]'>{(DeliveryFees == 7 ?"5+":DeliveryFees)}</span><span className=' w-[0.04rem] h-4 me-2 ms-2 bg-slate-400'></span></div> <MdOutlineKeyboardArrowDown size={17} onClick={OpenFees}/>
@@ -69,7 +69,8 @@ const Filter = () => {
                 {
                     (
                         Fees&&
-                            <form className=' fixed w-full px-3 pb-1 pt-5 shadow bg-white shadow-black rounded-lg  w-[25rem] space-y-2 z-10'onSubmit={calltheDeliveryfees} ref={dropdownRef}>
+                        <div className=' fixed w-full min-h-screen top-0 left-0 bg-black bg-opacity-50 flex items-center justify-center z-[999]'>
+                            <form className=' px-3 pb-1 pt-5 shadow bg-white shadow-black rounded-lg  w-[25rem] space-y-2 z-10'onSubmit={calltheDeliveryfees} ref={dropdownRef}>
                                 <h1 className=' font-semibold text-3xl'>Delivery Fees</h1>
                                 <p className=' text-[1rem] text-gray-500 font-semibold'>Delivery fees vary for each restaurant based on your location and other factors.</p>
                                 <div className=' text-[1rem] text-gray-500 font-semibold flex items-center'>Under <TbCurrencyDollar size={20}/><span className=' text-lg'>{(DeliveryFees==7?"5+":DeliveryFees)}</span> </div>
@@ -93,6 +94,7 @@ const Filter = () => {
                                     <button type='submit' className=' bg-primary px-5 py-2 rounded-full text-white' >View Result</button>
                                 </div>
                             </form>
+                        </div>
                     )
                 } 
             </div>
